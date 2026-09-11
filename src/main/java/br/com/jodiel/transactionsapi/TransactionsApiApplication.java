a5689a541ed63259fe05dccec389e2e036d978af
Jodiel Briesemeister <jodielll29@gmail.com>
2026-09-11 02:59:20 -0300

    chore: disable the unused default in-memory user

--- src/main/java/br/com/jodiel/transactionsapi/TransactionsApiApplication.java
+++ src/main/java/br/com/jodiel/transactionsapi/TransactionsApiApplication.java
@@ -2,9 +2,14 @@ package br.com.jodiel.transactionsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+/**
+ * Authentication is JWT only, so Boot's default in-memory user, and the generated password it logs at
+ * startup, would never be used.
+ */
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableScheduling
 public class TransactionsApiApplication {
 
